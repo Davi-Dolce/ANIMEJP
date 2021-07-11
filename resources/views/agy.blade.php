7ee8d6292214f594c794d3195385ed02d27255b7
Davi-Dolce <davidolce12@gmail.com>
2021-07-11 12:37:04 -0300

    Primeiro Commit

---
+++ resources/views/agy.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agy</title>
+</head>
+<body>
+    <h1>AGY é um site</h1>
+    <h2>Que era para ser "GAY"</h2>
+    <h3>MAs o programador escreveu errado</h3>
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AnimeJP</title>
+    <link rel="stylesheet" href="library/bootstrap-5.0.0-beta3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body class="tudo">
+    <header>
+        <nav class="navbar fixed-top justify-content-between">
+        <h1 class="col-lg-4"> Central Animes</h1>
+            <ul class="nav nav-pills col-lg-4">
+                <li class="nav-item">
+                    <a class="nav-link active" href="">Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="animes.html">Animes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="series.html">Series</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Sobre">Sobre</a>
+                </li>
+            </ul>
+        </nav>
+        <br><br><br><br> 
+        <h5 id="texto0">Neste site você vai encontrar os melhores animes da Crunchyroll e as melhores series da Netflix <br> para perder um bom tempo assistindo
+        </h5>
+        <br>
+    </header>
+    <main class="container" >
+    <br><br>
+        <div class="row justify-content-between textos">
+        <div class="col-lg-8 texto1">
+            <h4>Qualquer coisa</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam labore, eius error quibusdam facilis odit corporis expedita, cumque esse mollitia, distinctio officia laboriosam enim sed in velit assumenda earum? Saepe.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia corrupti similique quis tempora nobis doloremque modi ullam nam molestiae hic natus non laborum nihil, asperiores repellat distinctio officiis rerum ab.
+            </p>
+            <br><br><h4>Qualquer coisa 2</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam labore, eius error quibusdam facilis odit corporis expedita, cumque esse mollitia, distinctio officia laboriosam enim sed in velit assumenda earum? Saepe.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia corrupti similique quis tempora nobis doloremque modi ullam nam molestiae hic natus non laborum nihil, asperiores repellat distinctio officiis rerum ab.
+            </p>
+            <h5 class="text-center">Piririm pó</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam labore, eius error quibusdam facilis odit corporis expedita, cumque esse mollitia, distinctio officia laboriosam enim sed in velit assumenda earum? Saepe.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia corrupti similique quis tempora nobis doloremque modi ullam nam molestiae hic natus non laborum nihil, asperiores repellat distinctio officiis rerum ab.
+            </p>
+            <h5 class="text-center">Piririm pa</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam labore, eius error quibusdam facilis odit corporis expedita, cumque esse mollitia, distinctio officia laboriosam enim sed in velit assumenda earum? Saepe.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia corrupti similique quis tempora nobis doloremque modi ullam nam molestiae hic natus non laborum nihil, asperiores repellat distinctio officiis rerum ab.
+            </p>
+        </div>
+        <div class="col-lg-4">
+            <div class="texto2" id="tabela">
+               <table class="tabel">
+                   <tr>
+                       <th class="tabel">Anime</th>
+                       <th class="tabel">Visualizações</th>
+                       <th class="tabel">Episódios</th>
+                   </tr>
+                   <tr>
+                       <td class="tabel">Naruto</td>
+                       <td class="tabel">220M</td>
+                       <td class="tabel">600</td>
+                   </tr>
+                   <tr>
+                       <td class="tabel">Demon Slayer</td>
+                       <td class="tabel">40M</td>
+                       <td class="tabel">28</td>
+                   </tr>
+                   <tr>
+                       <td class="tabel">Nanatsu no Taiza</td>
+                       <td class="tabel">10M</td>
+                       <td class="tabel">100</td>
+                   </tr>
+                   <tr>
+                       <td class="tabel">Bleach</td>
+                       <td class="tabel">90M</td>
+                       <td class="tabel">366</td>
+                   </tr>
+                   <tr>
+                       <td class="tabel">Sword Art Online</td>
+                       <td class="tabel">20M</td>
+                       <td class="tabel">95</td>
+                   </tr>
+               </table>
+            </div>
+        </div>
+        </div>
+    </main>
+    <br><br><br>
+     <footer class="row" id="Sobre">
+        <div class="col-md-4 sociais tel">
+            <p>Aqui estão nossas redes sociais :</p>
+            <a href="" class="fa fa-facebook fa-lg face"> Facebook</a> <br>
+            <a href="" class="fa fa-instagram fa-lg insta"> Instagram</a> <br>
+            <a href="" class="fa fa-telegram fa-lg tele"> Telegram</a>
+        </div>
+        <div class="col-md-4">
+            <br>
+            <h4>Feito por SiniDolce</h4><br>
+            <p>Copyright © 2021 - by Sinidolce Intretenement</p>
+        </div>
+        <div class="col-md-4 text-center tel">
+            <p>Para mais informaçoes ou duvidas sobre o site <br> entre em contato com nosco :</p>
+            <p>(+55) 12 3833-7840</p>
+            <p>(+55) 12 93675-4702</p>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
